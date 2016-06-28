92656f8961958d866ecf38df2e1d988c219da976
Hobo86 <hobo.chb@gmail.com>
2016-06-28 16:31:24 +0800

    session flash 测试

--- routers/api/api.go
+++ routers/api/api.go
@@ -8,6 +8,7 @@ import (
 	"github.com/labstack/echo"
 	// "github.com/jinzhu/gorm"
 
+	"github.com/hobo-go/echo-mw/session"
 	"github.com/hobo-go/echo-web/modules/cache"
 )
 
@@ -15,6 +16,7 @@ func ApiHandler(c echo.Context) error {
 	idStr := c.Param("id")
 	id, err := strconv.ParseUint(idStr, 10, 64)
 
+	// 缓存测试
 	value := -1
 	if err == nil {
 		cacheStore := cache.Default(c)
@@ -26,20 +28,27 @@ func ApiHandler(c echo.Context) error {
 		}
 	}
 
+	// Flash测试
+	s := session.Default(c)
+	s.AddFlash("0")
+	s.AddFlash("1", "key")
+
 	request := c.Request()
 	c.JSON(http.StatusOK, map[string]interface{}{
-		"title":       "Api Index",
-		"CacheValue":  value,
-		"Scheme":      request.Scheme(),
-		"Host":        request.Host(),
-		"UserAgent":   request.UserAgent(),
-		"Method":      request.Method(),
-		"URI":         request.URI(),
-		"RemoteAddr":  request.RemoteAddress(),
-		"Path":        request.URL().Path(),
-		"QueryString": request.URL().QueryString(),
-		"QueryParams": request.URL().QueryParams(),
-		"HeaderKeys":  request.Header().Keys(),
+		"title":        "Api Index",
+		"CacheValue":   value,
+		"Scheme":       request.Scheme(),
+		"Host":         request.Host(),
+		"UserAgent":    request.UserAgent(),
+		"Method":       request.Method(),
+		"URI":          request.URI(),
+		"RemoteAddr":   request.RemoteAddress(),
+		"Path":         request.URL().Path(),
+		"QueryString":  request.URL().QueryString(),
+		"QueryParams":  request.URL().QueryParams(),
+		"HeaderKeys":   request.Header().Keys(),
+		"FlashDefault": s.Flashes(),
+		"Flash":        s.Flashes("key"),
 	})
 
 	return nil
