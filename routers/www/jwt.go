39b4bb6ae453dbeef88feda251a7370dab4b9432
Hobo86 <hobo.chb@gmail.com>
2016-07-30 18:26:11 +0800

    update template

--- routers/www/jwt.go
+++ routers/www/jwt.go
@@ -5,7 +5,7 @@ import (
 )
 
 func JWTTesterHandler(c echo.Context) error {
-	c.Set("tmpl", "api/jwt_tester")
+	c.Set("tmpl", "www/jwt_tester")
 	c.Set("data", map[string]interface{}{
 		"title": "JWT 接口测试",
 		"token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6dHJ1ZSwiaWQiOiIxIiwibmFtZSI6IkhvYm8ifQ.YUzBykoELyKoQWaugkVNf3d09HBhICBJoOcWQKnveRQ",
